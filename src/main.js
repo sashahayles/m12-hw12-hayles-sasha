1326fa136721c521f55b029ebdd37e8018b4deff
Sasha Hayles <sashahayles@yahoo.com>
2019-11-11 22:22:18 -0500

    Add classes and subclasses for JS

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
-class Vehicle  {
-  constructor(size, year) {
-    this.size = size;
-    this.year = year;
-    console.log('Vroom');
+class Desserts  {
+  constructor(type, calories) {
+    this.type = type;
+    this.calories = calories;
   }
 }
-const truck = new Vehicle ('big', 2008);
-console.log(truck);
-
-class Truck extends Vehicle {
-  constructor(size, year, make, model) {
+class IceCream extends Desserts {
+  constructor(type, calories, flavor, scoops) {
     super(size, year);
     this.make = make;
     this.model =model;
   }
+  ecoFriendly () {
+    if (this.size === 'Big') {
+      console.log ('Buy a Prius!');
+    }
+  }
 }
+
+const chevySilverado = new Truck ('Big', 2008, 'Chevrolet', 'Silverado');
+console.log (chevySilverado.year);
+chevySilverado.ecoFriendly();
